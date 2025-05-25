d07aeefef40b8faf1b3590bcee7ea54247915090
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-05-24 19:48:01 -0700

    adding formatting

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -9,37 +9,37 @@ import (
 
 func TestAPIKey(t *testing.T) {
 	tests := []struct {
-		key string
-		value string 
-		expect string
+		key       string
+		value     string
+		expect    string
 		expectErr string
 	}{
 		{
-		expectErr: "no authorization header",
-	}, 
-	{
-		key: "Authorization",
-		value: "-",
-		expectErr: "malformed auth",
-	},
-}
-for i, test := range tests {
-	t.Run(fmt.Sprintf("TestAPIKey Case #%v:", i), func(t *testing.T) {
-		header := http.Header{}
-		header.Add(test.key, test.value)
+			expectErr: "no authorization header",
+		},
+		{
+			key:       "Authorization",
+			value:     "-",
+			expectErr: "malformed auth",
+		},
+	}
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("TestAPIKey Case #%v:", i), func(t *testing.T) {
+			header := http.Header{}
+			header.Add(test.key, test.value)
 
-		output, err := GetAPIKey(header)
-		if err != nil {
-			if strings.Contains(err.Error(), test.expectErr) {
+			output, err := GetAPIKey(header)
+			if err != nil {
+				if strings.Contains(err.Error(), test.expectErr) {
+					return
+				}
+				t.Errorf("Unexpected test case error: %v", err)
 				return
 			}
-			t.Errorf("Unexpected test case error: %v", err)
-			return
-		}
-		if output != test.expect {
-			t.Errorf("Undexpected: TestGetAPIKey: %v", output)
-			return 
-		}
-	})
-}
+			if output != test.expect {
+				t.Errorf("Undexpected: TestGetAPIKey: %v", output)
+				return
+			}
+		})
+	}
 }
